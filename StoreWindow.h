6c551ab30b5f9c1cae3122f005526336cbcba61a
gannaradwan45-ux <gannaradwan45@gmail.com>
2026-05-05 13:19:14 +0300

    Update StoreWindow.h

--- StoreWindow.h
+++ StoreWindow.h
@@ -1,103 +1,43 @@
 #pragma once
-#include <string>
-#include <vector>
-    using namespace std;
+#include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QGridLayout>
+#include <QScrollArea>
+#include <QComboBox>
+#include <QMenu>
+#include <QAction>
+#include "Inventory.h"
+#include "ShoppingCart.h"
+#include "NotifPanel.h"
 
-class CartItem {
+class StoreWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    StoreWindow(Inventory* inv, ShoppingCart* cart,
+                QString username, QWidget* parent = nullptr);
+
+private slots:
+    void onCategoryChanged(int index);
+    void onAddToCart(int productId);
+    void onViewCart();
+    void onLogout();
+
 private:
-    int productID;
-    string productName;
-    double price;
-    int quantity;
-    double subtotal;
+    Inventory* inventory;
+    ShoppingCart* cart;
+    QString username;
 
-public:
-    CartItem(int pid, string pname, double pprice, int qty);
-    ~CartItem();
+    QWidget* productsWidget;
+    QGridLayout* productsGrid;
+    QLabel* cartCountLabel;
+    NotifPanel* notifPanel;
+    QPushButton* notifBtn;
+    int notifCount;
 
-    int getProductID() const;
-    string getProductName() const;
-    double getPrice() const;
-    int getQuantity() const;
-    double getSubtotal() const;
-
-    void setQuantity(int qty);
-
-    void display() const;
-    void display(bool showSubtotal) const;
-};
-
-class ShoppingCart {
-private:
-    int cartID;
-    int customerID;
-    vector<CartItem> items;
-    double totalAmount;
-
-    void recalculateTotal();
-
-public:
-    ShoppingCart(int cid, int custID);
-    ~ShoppingCart();
-
-    void addItem(int productID, string productName, double price, int quantity);
-    void removeItem(int productID);
-    void updateQuantity(int productID, int newQty);
-    void clearCart();
-
-    int getCartID() const;
-    int getCustomerID() const;
-    double getTotalAmount() const;
-    vector<CartItem> getItems() const;
-    bool isEmpty() const;
-    int getQuantity(int productID) const;  // NEW
-
-    void display() const;
-    void display(string header) const;
-};
-
-class Order {
-private:
-    int orderID;
-    int customerID;
-    string status;
-    double totalPrice;
-    string orderDate;
-
-public:
-    Order(int id, int custID, ShoppingCart& cart, string date);
-    Order(int id, int custID, double total, string date);
-    ~Order();
-
-    int getOrderID() const;
-    string getStatus() const;
-    double getTotalPrice() const;
-
-    void setStatus(string s);
-    void cancelOrder();
-    void trackOrder() const;
-
-    void display() const;
-    void display(bool detailed) const;
-};
-
-class RecordKeeper {
-protected:
-    int ownerID;
-public:
-    RecordKeeper(int id);
-    virtual ~RecordKeeper();
-    virtual void displayAll() const = 0;
-};
-
-class OrderHistory : public RecordKeeper {
-private:
-    vector<Order> orders;
-public:
-    OrderHistory(int customerID);
-    ~OrderHistory();
-
-    void addOrder(Order& order);
-    void displayAll() const override;
-    void displayAll(string filterStatus) const;
+    void loadProducts(QString category);
+    QWidget* createProductCard(int id, QString name,
+                               double price, int stock,
+                               QString category);
 };
