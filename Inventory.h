e58875cfc6beb6ee57ff2612f95bba2d265b5ac5
gannaradwan45-ux <gannaradwan45@gmail.com>
2026-05-05 23:26:02 +0300

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -9,23 +9,27 @@ private:
     vector<Product*> products;
     int capacity;
     int currentCount;
+    string filename;
 
 public:
-    Inventory(int cap);
+    Inventory(int cap, string file = "products_data.txt");
     ~Inventory();
 
     void addProduct(Product* product);
     void addProduct(string name, double price, string category, int quantity, int vendorID);
+
+    void saveToFile();
+    void loadFromFile();
+
     Product* getProduct(int productID);
     vector<Product*> getAllProducts();
     vector<Product*> getProductsByCategory(string category);
     vector<Product*> getProductsByVendor(int vendorID);
     bool isProductAvailable(int productID, int quantity);
     void updateStock(int productID, int newQuantity);
-
-    // New methods for admin
-    void updateProduct(int productID, double newPrice, int newStock);  // Add this
-    bool removeProduct(int productID);  // Add this
-
+    void updateProduct(int productID, double newPrice, int newStock);
+    bool removeProduct(int productID);
     bool removeProductByVendor(int productID, int vendorID);
+
+    int getCurrentCount() const { return currentCount; }
 };
